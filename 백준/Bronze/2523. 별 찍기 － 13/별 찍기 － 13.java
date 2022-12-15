c92173ae12aacaeb5dc7dd380010f4c267157406
ImJaePang <118138003+ImJaePang@users.noreply.github.com>
2022-12-15 18:05:43 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 372 ms, Memory: 18872 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        
+        for (int i = 1; i <= num ; i++){
+            for (int j = 1; j <= i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= num -1 ; i++){
+            for (int j = 1; j <= num - i; j++){
+                System.out.print("*");
+            }
+            if (i != num-1){
+                System.out.println();
+            }
+        }
+    }
+}
