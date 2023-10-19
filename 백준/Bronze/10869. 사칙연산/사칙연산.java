f0da913449c0b7e7f096dc1298fa41d728fd6919
IM JAEYOUNG <118138003+ImJaePang@users.noreply.github.com>
2023-10-19 17:14:53 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,16 +1,14 @@
-import java.util.Scanner;
+import java.util.*;
 
 class Main{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         int b = scan.nextInt();
-        
         System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a*b);
         System.out.println(a/b);
-        System.out.println(a%b);
-        
+        System.out.println(a%b);       
     }
 }
