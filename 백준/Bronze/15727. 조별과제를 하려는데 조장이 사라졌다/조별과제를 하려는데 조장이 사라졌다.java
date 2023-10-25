fb8169c00042175fd91ca66af710c8d7a2085fa9
IM JAEYOUNG <118138003+ImJaePang@users.noreply.github.com>
2023-10-25 17:12:16 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 120 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -1,13 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 class Main{
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
-        if (num % 5 == 0) {
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        if (num % 5 == 0){
             System.out.println(num / 5);
         } else {
-            System.out.println(num / 5 +1);            
-        }        
+            System.out.println(num / 5 + 1);
+        }
+            
+            
+        
     }
 }
