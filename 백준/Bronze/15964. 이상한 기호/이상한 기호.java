2ee8c8710116417e5624bfeebbc97363d15ccfc6
IM JAEYOUNG <118138003+ImJaePang@users.noreply.github.com>
2023-10-26 10:00:28 +0900

    [Bronze V] Title: 이상한 기호, Time: 128 ms, Memory: 14368 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -1,13 +1,14 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.math.BigInteger;
 
 class Main{
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int num0 = Integer.parseInt(st.nextToken());
-        int num1 = Integer.parseInt(st.nextToken());
-        System.out.println((num0+num1)*(num0-num1));
+        BigInteger num0 = new BigInteger(st.nextToken());
+        BigInteger num1 = new BigInteger(st.nextToken());
+        System.out.println(num0.add(num1).multiply(num0.subtract(num1)));
     }
 }
